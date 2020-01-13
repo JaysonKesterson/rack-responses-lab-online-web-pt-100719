b20d1e68b606a57d995d578a4bedcc6e2bdb8f4a
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-01-13 17:24:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,13 @@ class Application
   def call(env)
     resp = Rack::Response.new
     
+    what_time_is_it = Time.now
+    if what_time_is_it > 12:00:00 
+      resp.write "Good Afternoon"
+    else
+      resp.write "Good Morning"
+    end
+    
   end
   
 end
